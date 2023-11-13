6160c9dc432c835682350e2a4dd1607dc104f0e6
romanasbananas <societyorenb@mail.ru>
2023-11-13 20:41:27 +0300

    Please Help

--- 1  task chapter2.py
+++ 1  task chapter2.py
@@ -1,10 +1,13 @@
-number =int(input('Введите Число блять'))
+number = int(input('Введите Число блять'))
 num = number // 2
-k = 1
+k = 2
 while k < num:
-    if number // k == 0:
-        print('Это ахуеть какое простое число')
-    else:
-        print('Это не очень то и простое число')
+    if number % k == 0:
+        print('Это число не является простым')
         break
+    else:
+        k = k+1
+
+else:
+    print("Это простое число")
 print('Программа завершена')
