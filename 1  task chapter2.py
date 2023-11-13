e2ee9079e45cf288d8af6eaf057a9513ae8ca83a
romanasbananas <societyorenb@mail.ru>
2023-11-13 21:42:32 +0300

    Please Help

--- 1  task chapter2.py
+++ 1  task chapter2.py
@@ -1,8 +1,8 @@
-number = int(input('Введите число'))
-print('Число делится на :'+str(1))
-k = 1
-while k < number//2:
-    if number %k!=0:
-        print('Делится на,', k)
-    k = k+1
-print('Число делится на :', number)
+nums = int(input('Введите Число'))
+range = range(0,10)
+k = ()
+for s in range:
+    if int(9) in nums:
+        k = ()
+        continue
+    print(k)
