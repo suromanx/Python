436453a72c4f662a9ac0f1f30deee8b00afec821
romanasbananas <societyorenb@mail.ru>
2023-11-17 10:05:32 +0300

    OH MY GOD;

--- just.py
+++ just.py
@@ -2,13 +2,12 @@
 number = int(input('VV'))
 num = number // 2
 k = 2
-while k < num:
-    for k in range (k,num):
-        if number % k == 0:
-            print ('не простое')
-            raise SystemExit(0)
-        else:
-            print('простое')
-            raise SystemExit(0)
+while k <= num:
+   if number % k == 0:
+       print ('не простое')
+       break
+   k =+ 1
+else:
+    print('простое')
 
 print('the end')
