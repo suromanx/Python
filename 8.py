1f2d69b71dd28e54c6c11ea05616c7057b4bed49
romanasbananas <societyorenb@mail.ru>
2023-11-20 15:14:17 +0300

    Day of the week

--- 8.py
+++ 8.py
@@ -1,5 +1,12 @@
-a, b, c = map(int,input('VVedite').split())
-if c - b == b - a:
-    print('Этот список является последовательностью')
-else:
-    print("Это не последовательность") ,
+number = input('Введите число')
+day = {1:'Понедельник',2:'Вторник',3:'Среда',4:'Четверг', 5:'Пятница', 6:'Суббота', 7:'Воскресенье'}
+result = ''
+for i in number:
+    if i.isdigit():
+        digit = int(i)
+        if 1 <= digit <= 7:
+            result += day[digit] + ' '
+        else:
+            result += 'unknown'
+
+print(result)
