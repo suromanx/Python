8327e8016b47330a46e60fad1f420658ad2e1409
romanasbananas <societyorenb@mail.ru>
2023-11-30 14:52:44 +0300

    stupid pupid 2

--- 123.py
+++ 123.py
@@ -1,21 +1,5 @@
-def create_tuple(text):
-    original_tuple = tuple(text)
-    return original_tuple
+number = int(input('Input the number'))
+number_tuple = tuple(map(int,str(number)))
 
-def create_new_tuple(original_tuple,step):
-    new_tuple = original_tuple[::step]
-    return new_tuple
-
-def main():
-     user_text = input('Input the text:')
-     step = int(input('Input step:'))
-
-     original_tuple = create_tuple(user_text)
-     new_tuple = create_new_tuple(original_tuple,step)
-
-
-     print('Original tuple:',original_tuple)
-     print('New tuple:', new_tuple)
-
-if __name__ == '__main__':
-    main()
+print('Number Tuple:', number_tuple)
+print('Convert Tuple', number_tuple[::-1])
