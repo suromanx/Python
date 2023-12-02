b9fc34c0cdda6ee60f98c645be15cd5a343196aa
romanasbananas <societyorenb@mail.ru>
2023-12-02 20:47:23 +0300

    tuple creation

--- 123.py
+++ 123.py
@@ -1,5 +1,22 @@
-number = int(input('Input the number'))
-number_tuple = tuple(map(int,str(number)))
+def create_tuple(user_text):
+    original_tuple = tuple(user_text)
+    return original_tuple
 
-print('Number Tuple:', number_tuple)
-print('Convert Tuple', number_tuple[::-1])
+def the_new_tuple(original_tuple, step):
+    new_tuple = original_tuple[::step]
+    return  new_tuple
+
+def main():
+    user_text = input('Input number')
+    step = int(input('Input step'))
+
+    original_tuple = create_tuple(user_text)
+    new_tuple = the_new_tuple(original_tuple,step)
+
+
+    print('The original tuple:', original_tuple)
+    print('New tuple:', new_tuple)
+
+
+if __name__ == '__main__':
+    main()
