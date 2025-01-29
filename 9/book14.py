0f3d05c83be67e3e82435820b9653cc2c89e0b0d
Roman Sukhoruchkin <societyorenb@mail.ru>
2025-01-29 11:10:40 +0300

    29 jan

--- 9/book14.py
+++ 9/book14.py
@@ -3,3 +3,19 @@ class Alpha:
         return len(name)
 
 class Bravo:
+    def show(self,x):
+        print('Method show(): ', x)
+
+    def __getattr__(self, name):
+        if len(name)<5:
+            return lambda x:print('First method:',x)
+        else:
+            return lambda x: print('Второй метод',x*x)
+
+print('Обьект А класса Alpha')
+A = Alpha()
+A.value = 'Alpha'
+print(A.value)
+print()
+print(A.color)
+print('Field myattribute:', A.myattribute)
